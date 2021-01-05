d769e8278c9153ec6a408f55127b973159599738
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-01-04 21:49:43 -0800

    updates

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,61 +1,16 @@
-USE employeesDB;
+  
+INSERT INTO department (dept_name) VALUES ('Back of House'), ('Front of House'), ('Management');
+INSERT INTO company_role (title, salary, dept_id) VALUES
+('General Manager', 80000.00, 3), 
+('Head Chef', 60000.00, 1),
+('Sous Chef', 30000.00, 1),
+('Cook', 30000.00, 1 ),
+('Dishwasher', 20000.00, 1),                
+('Front of House Manager', 40000.00, 2),
+('Host', 30000.00, 2),
+('Server', 20000.00, 1);
 
-/* === || DEPARTMENT ARRAY || === */
-INSERT INTO department (name)
-VALUES ("Sales");
-INSERT INTO department (name)
-VALUES ("Engineering");
-INSERT INTO department (name)
-VALUES ("Finance");
-INSERT INTO department (name)
-VALUES ("Legal");
-
-/* === || ROLE ARRAY || === */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1);
-/* Sales Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 80000, 1);
-/* Sales Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
-/* Engineering Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
-/* Engineering Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Account Manager", 150000, 3);
-/* Accounting Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 125000, 3);
-/* Accounting Department */
-INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 4);
-/* Legal Department */
-
-
-/* === || EMPLOYEE ARRAY || === */
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* John Doe - Sales Lead - Sales Department */
-VALUES ("John", "Doe", 1, null); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* Ryan Reynolds - Sales Lead - Sales Department */
-VALUES ("Ryan", "Reynolds", 1, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* Jane Doe - Salesperson - Sales Department */
-VALUES ("Jane", "Doe", 2, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* Joshua Small - Lead Engineer - Engineering Department */
-VALUES ("Joshua", "Small", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* Maggie Small - Software Engineer - Engineering Department */
-VALUES ("Maggie", "Small", 4, 4);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* George Clooney - Account Manager- Accounting Department */
-VALUES ("George", "Clooney", 5, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* James Brown - Accountant - Accounting Department */
-VALUES ("James", "Brown", 6, 6);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-/* Tom Allen - Leagl Team Lead - Legal Department */
-VALUES ("Tom", "Allen", 7, null);
+INSERT INTO employees (first_name, last_name, emp_role_id, manager_id) VALUES
+('Mark', 'Danzino', 1, null),
+('Abigail', 'Jenner', 6, 1),
+('Derek', 'Fisher', 2, null);
